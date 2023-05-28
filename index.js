a71904c3ec1a8fdeb7db73be7ab943d35eaf9f8a
mohammedthayyib <mohammedthayyibdev@gmail.com>
2023-05-28 18:59:36 +0530

    added Mongodb as database using mongoose

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 
-app.use(express.json())//used to access request body, without this line we wont be able to access request body, it will be an empty object - this is a middleware -used to convert the request from json to object
+app.use(express.json())
+//used to access request body, without this line we wont be able to access request body, it will be an empty object - this is a middleware -used to convert the request from json to object
 
 morgan.token('req', function (req, res) { 
 	return JSON.stringify(req.body)
@@ -15,31 +17,37 @@ app.use(morgan(':method :url :status :req'));
 app.use(cors())
 app.use(express.static('build'))//this middleware allows us to use the minified code for user level use and we no more have to start the react file with npm start , starting the server is only needed
 
+if(process.argv.length < 3) {
+    console.log("give password as argument");
+    process.exit(1)
+}
 
-let notes = [
-		{
-			id: 1,
-			content: "HTML is easy",
-			important: true
-		},
-		{
-			id: 2,
-			content: "Browser can execute only JavaScript",
-			important: false
-		},
-		{
-			id: 3,
-			content: "GET and POST are the most important methods of HTTP protocol",
-			important: true
-		},
-		{
-			id: 4,
-			content: "new content",
-			important: true
-		}
-	]
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+const password = process.argv[2]
 
+const url = `mongodb+srv://mohammedthayyibdev:${password}@cluster1.tpiudbx.mongodb.net/noteApp?retryWrites=true&w=majority`
 
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+// defining schema
+const noteSchema = new mongoose.Schema({
+	content: String,
+	important: Boolean,
+})
+
+// changing the cofigurable options using set method of the schema to exclude the __v field and _id field getting send to the user
+// by transforming the toJSON method
+noteSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()//this conversion of id to string is needed for later use
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
+})
+
+// defining model
+const Note = mongoose.model('Note', noteSchema)
 
 const generateId = () => {
 	const maxId = notes.length > 0 ?
@@ -70,7 +78,11 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-	response.json(notes)
+	// when we get a GET request at /api/notes , we fetch the notes from the database using Note model
+	Note.find({}).then(notes => {
+		// console.log(notes);
+		response.json(notes)
+	})
 })
 
 
